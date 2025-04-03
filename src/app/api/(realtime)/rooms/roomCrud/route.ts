01b8fb413d9b3a5da68dccf3725bb94cf51d395e
hayasin2004 <masataka1kousuke1@gmail.com>
2025-04-03 14:00:01 +0900

    /roomsでルーム作成完了

--- src/app/api/(realtime)/rooms/roomCrud/route.ts
+++ src/app/api/(realtime)/rooms/roomCrud/route.ts
@@ -66,6 +66,7 @@ async function generateRoomItems(roomId: number) {
             const x = Math.floor(Math.random() * 580) + 10;
             const y = Math.floor(Math.random() * 580) + 10;
 
+
             roomItems.push({
                 roomId,
                 itemId: item.id,
