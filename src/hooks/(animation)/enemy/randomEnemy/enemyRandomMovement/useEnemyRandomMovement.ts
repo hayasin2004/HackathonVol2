9c243fe07b94344f9ca7daed9e3d896bda87e798
hayasin2004 <masataka1kousuke1@gmail.com>
2025-04-25 14:33:50 +0900

    敵が動きます。randomは完成

--- src/hooks/(animation)/enemy/randomEnemy/enemyRandomMovement/useEnemyRandomMovement.ts
+++ src/hooks/(animation)/enemy/randomEnemy/enemyRandomMovement/useEnemyRandomMovement.ts
@@ -11,10 +11,22 @@ const useEnemyRandomMovement = (initialX: number, initialY: number, type: string
     useEffect(() => {
         if (type === "random") {
             const intervalId = setInterval(() => {
-                setPosition(prevPosition => ({
-                    x: prevPosition.x + (Math.random() > 0.5 ? 64 : -64),
-                    y: prevPosition.y + (Math.random() > 0.5 ? 64 : -64),
-                }));
+                setPosition(prevPosition => {
+                    // 現在のウィンドウの幅と高さを取得
+                    const screenWidth = window.innerWidth;
+                    const screenHeight = window.innerHeight;
+
+                    // 新しい位置を計算
+                    let newX = prevPosition.x + (Math.random() > 0.5 ? 64 : -64);
+                    let newY = prevPosition.y + (Math.random() > 0.5 ? 64 : -64);
+                    console.log(newX, newY);
+
+                    if (newX < 0) newX = 0;
+                    if (newY < 0) newY = 0;
+                    if (newX >  screenWidth - 64) newX = screenWidth - 64;
+                    if (newY >  screenHeight - 64) newY = screenHeight - 64;
+                    return {x :newX  , y :newY}
+                });
             }, 1000); // 1秒ごとに動かす
 
             return () => clearInterval(intervalId);
